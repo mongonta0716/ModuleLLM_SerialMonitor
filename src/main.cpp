8ed4ab0a335a913a89d07e2888c342ca9ad427a9
Takao Akaki <mongonta@gmail.com>
2025-02-05 17:06:28 +0900

    simple

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #include <Arduino.h>
 #include <M5Unified.h>
 
-void setup() {
+void setup() { 
+    auto cfg = M5.config();
+    M5.begin(cfg);
+    M5.Display.setFont(&fonts::lgfxJapanMincho_16);
+    M5.Lcd.print("Hello, M5Stack!");
+    Serial2.begin(115200, SERIAL_8N1, 18, 17);
 
 }
 
 void loop() {
-    
+    if (Serial2.available()) {
+        String input = Serial2.readString();
+        M5.Display.println(input);
+    }
 }
